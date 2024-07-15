df7ab45c55ee3065512293d927ee3dd9c145039b
j3rryl <jeremymunroe0@gmail.com>
2024-07-15 16:24:16 +0300

    update unit tests

--- tests/Feature/MediaControllerTest.php
+++ tests/Feature/MediaControllerTest.php
@@ -16,13 +16,60 @@ class MediaControllerTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+    public function test_get_all_media(): void
     {
-        $response = $this->get('/');
-
+        $response = $this->get('/api/media');
         $response->assertStatus(200);
     }
-    public function testUpdateMediaWithFileUpload()
+    public function test_post_media()
+    {
+        Storage::fake("local");
+        // Create a sample genre record
+        $genre = Genre::create([
+            'name' => 'Sample Genre',
+            'description' => 'Sample Description',
+        ]);
+
+        // Simulate a file upload
+        $file = UploadedFile::fake()->image('thumbnail.jpg');
+
+        $data = [
+            'name' => 'New Media Name',
+            'genre_id' => $genre->id,
+            'description' => 'New Description',
+            'thumbnail' => $file,
+            'media_type' => 'music',
+        ];
+
+        // Send the update request
+        $response = $this->json('POST', route('media.store'), $data);
+        if (!$response->isSuccessful()) {
+            dump($response->getContent()); 
+        }
+        $media = $response->json('media');
+        $response->assertStatus(201);
+        // Assert the response JSON structure
+        $response->assertJson([
+            'message' => 'Media created successfully',
+            'media' => [
+                'id' => $media['id'],
+                'name' => 'New Media Name',
+                'genre_id' => $genre->id,
+                'description' => 'New Description',
+                'media_type' => 'music',
+            ],
+        ]);
+
+        Storage::disk("local")->assertExists("media_thumbnails/".$file->hashName());
+        // Assertion Update was successful
+        $this->assertEquals('New Media Name', $media['name']);
+        $this->assertEquals($genre->id, $media['genre_id']);
+        $this->assertEquals('New Description', $media['description']);
+        $this->assertEquals("media_thumbnails/".$file->hashName(), $media['thumbnail']);
+        $this->assertEquals('music', $media['media_type']);
+    }
+
+    public function test_update_media()
     {
         Storage::fake("local");
 
@@ -53,8 +100,7 @@ class MediaControllerTest extends TestCase
 
         // Send the update request
         $response = $this->json('PUT', route('media.update', $media->id), $data);
-        if ($response->status() !== 200) {
-            info('Response Body: '.$response->json());
+        if (!$response->isSuccessful()) {
             dump($response->getContent()); 
         }
         $media->refresh();
@@ -79,5 +125,30 @@ class MediaControllerTest extends TestCase
         $this->assertEquals('New Description', $media->description);
         $this->assertEquals("media_thumbnails/".$file->hashName(), $media->thumbnail);
         $this->assertEquals('music', $media->media_type);
+    }
+
+    public function test_delete_media()
+    {
+        $media = Media::create([
+            'name' => 'Old Media Name',
+            'genre_id' => null,
+            'description' => 'Old Description',
+            'thumbnail' => null,
+            'media_type' => 'movie',
+        ]);
+
+        // Send the delete request
+        $response = $this->json('DELETE', route('media.destroy', $media->id));
+
+        if (!$response->isSuccessful()) {
+            info('Response Body: '.$response->json());
+            dump($response->getContent()); 
+        }
+        
+        $response->assertStatus(200);
+        $this->assertDatabaseMissing('media', ['id' => $media->id]);
+        $response->assertJson([
+            'message' => 'Media deleted successfully'
+        ]);
     }
 }
